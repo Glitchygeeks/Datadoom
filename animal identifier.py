9a05bbe30070a204fddbc748da392bb66e2298f4
Glitchygeeks <143184410+Glitchygeeks@users.noreply.github.com>
2025-03-11 17:03:06 +0530

    Create animal identifier.py

---
+++ animal identifier.py
@@ -0,0 +1,38 @@
+print("welcome to animal identifier")
+i=input("enter if your animal has visible pore on its surface")
+if i=="yes":
+    print("your animal is poriferan")
+else:
+    j=input('enter if your animal has tentacles with umbrella like structure or is stuck like a plant')
+if j=="yes":
+    print('your animal is cnidarian')
+else:
+    k=input('enter if your animal has high regenerative power and has no anus')
+if k=="yes":
+    print('your animal is a platyhelminth')
+else:
+    l=input('enter if your animal is a endoparasite and looks like a tube with both mouth and anus which looks different in male and female')
+if l=="yes":
+    print("your animal is aschhelminth")
+else:
+    m=input('enter if your animal looks segmented')
+if m=="yes":
+    print('your animal is annelid')
+else:
+    n=input('enter if your animal looks bony from outside and has jointed legs')
+if n=="yes":
+    print("your animal is an arthropod")
+else:
+    o=input('enter if your animal has a shell')
+if o=="yes":
+    print('your animal is a molluscs')
+else:
+    p=input('enter if your animal has radial(star or spherical) shape')
+if p=="yes":
+    print("your animal is an echinoderm")
+else:
+    q=input('enter if your animal has proboscis,collar,trunk and look like a big worm without a backbone')
+if q=="yes":
+    print('your animal is a hemichordate')
+else:
+    print('your animal is chordate')
